8702ca4aab5afc1c49fce3a814d89c0485ec28f7
Michael Ellicott <mellic03@gmail.com>
2023-08-10 14:36:49 +1000

    README

---
+++ docs/README.md
@@ -0,0 +1,14 @@
+# No-name Kernel
+This is a small side project of mine, I haven't named it yet.
+
+
+
+## Features
+
+- [ ] Subset of C standard library
+
+- [ ] Subset of C++ standard library
+
+- [ ] 3D graphics engine
+    - [x] Fixed-point math
+
--- src/system/kernel/entry.asm
+++ src/system/kernel/entry.asm
@@ -1,20 +1,20 @@
 section .text
-    ; global enable_sse
+    global enable_sse
     global _start
     extern main
 
-; enable_sse:
-;     fninit
+enable_sse:
+    fninit
 
-;     mov  rax,  cr0
-;     and  ax,   0xFFFB
-;     or   ax,   0x2
-;     mov  cr0,  rax
-;     mov  rax,  cr4
-;     or   ax,   3 << 9
-;     mov  cr4,  rax
-;     ret
-
+    mov  rax,  cr0
+    and  ax,   0xFFFB
+    or   ax,   0x2
+    mov  cr0,  rax
+    mov  rax,  cr4
+    or   ax,   3 << 9
+    mov  cr4,  rax
+    ret
+ 
 _start:
     ; call enable_sse
     call main
